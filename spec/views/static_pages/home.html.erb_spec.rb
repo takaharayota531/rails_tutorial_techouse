c88cb672324cf52d0d021b0bc8acdcfe813b71bd
takaharayota531 <takahara.yota@gmail.com>
2023-05-15 15:50:59 +0900

    first

---
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -0,0 +1,8 @@
+# require 'rails_helper'
+
+# RSpec.describe "static_pages/home.html.erb", type: :view do
+#  it "renders the correct page titie" do
+#    render
+#    expect(rendered).to have_title("Home")
+#  end
+# end
